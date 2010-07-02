d9a120f8d31183e94619e855eb7b811528331f45
florinail <florinail@5e4c1358-b78c-702d-5bc4-760e88c5cea0>
2010-07-02 08:22:26 +0000

    commit

--- iMallLiferant/src/paper4all/tests/ConverterTests.java
+++ iMallLiferant/src/paper4all/tests/ConverterTests.java
@@ -1,7 +1,13 @@
 package paper4all.tests;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.Reader;
 
 import org.jruby.embed.LocalContextScope;
 import org.jruby.embed.PathType;
@@ -10,20 +16,33 @@ import org.jruby.embed.ScriptingContainer;
  
 public class ConverterTests {
 	
-	private final static String filename = "xml2edi.rb";
+	private final static String filename = "edi2xml.rb";
  
 	private ConverterTests() 
 	{
+		try
+		{
 		//System.out.println("[" + getClass().getName() + "]");		
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		PrintStream ps = new PrintStream(baos);
-		
-		ScriptingContainer container = new ScriptingContainer(LocalContextScope.SINGLETHREAD);
-		container.setArgv(new String[]{"edi/in1.xml"});
-		container.setOutput(ps);
-		container.runScriptlet(PathType.CLASSPATH, filename);
-		String content = baos.toString();
-		System.out.println(content);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintStream ps = new PrintStream(baos);
+			
+			ScriptingContainer container = new ScriptingContainer(LocalContextScope.SINGLETHREAD);
+			container.setArgv(new String[]{"edi/in2.edi"});
+			//container.setOutput(ps);
+		    BufferedReader stdin =
+		        new BufferedReader( new FileReader("edi/in2.edi"));
+	
+			container.runScriptlet(stdin, filename);
+			//container.runScriptlet(PathType.CLASSPATH, filename);
+			String content = baos.toString();
+			System.out.println(content);
+			baos.close();
+			stdin.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 		
 	}
 	 
