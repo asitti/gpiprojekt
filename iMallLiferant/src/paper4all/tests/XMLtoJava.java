30260da319802ae940cb830581342ce17cf44ba8
florinail <florinail@5e4c1358-b78c-702d-5bc4-760e88c5cea0>
2010-07-02 09:48:16 +0000

    from xml to java classes

---
+++ iMallLiferant/src/paper4all/tests/XMLtoJava.java
@@ -0,0 +1,44 @@
+package paper4all.tests;
+
+import java.io.File;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import paper4all.rubyParser.Interchange;
+import paper4all.rubyParser.Message;
+
+public class XMLtoJava 
+{
+
+	public static void main(String[] args)
+	{
+		try 
+		{
+			JAXBContext jc = JAXBContext.newInstance("paper4all.rubyParser");
+			Unmarshaller u = jc.createUnmarshaller();
+		    Interchange interchange = (Interchange ) u.unmarshal(new File("edi/in1.xml"));
+		    if(interchange != null)
+		    {
+		    	List<Object> msg = interchange.getMessageOrMsgGroup();
+		    	for(Object o : msg)
+		    	{
+		    		if(o instanceof Message)
+		    		{
+		    			Message m = (Message)o;
+		    			System.out.println("message trailer:" + m.getTrailer().getSegment().getName());
+		    		}
+		    	}
+		    	System.out.println(interchange.getMessageOrMsgGroup().size());
+		    	System.out.println(interchange.getTrailer().getSegment().getName());
+		    }
+		} 
+		catch (JAXBException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
