7e8ce74fd21218df544dd83f4cafca3c53017cf7
florinail <florinail@5e4c1358-b78c-702d-5bc4-760e88c5cea0>
2010-07-12 15:21:31 +0000

    reading from the hub

--- trunk/iMallLiferant/src/paper4all/services/ReadInbox.java
+++ trunk/iMallLiferant/src/paper4all/services/ReadInbox.java
@@ -1,13 +1,19 @@
 package paper4all.services;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FilenameFilter;
+import java.io.OutputStream;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPListParseEngine;
 
 @WebService(name="ReadInboxWebService") 
 @SOAPBinding(style = SOAPBinding.Style.DOCUMENT) 
@@ -18,7 +24,53 @@ public class ReadInbox
 	@WebResult(name = "filename-result") 
 	public String fileToProcess() 
 	{ 
-		File f = new File("in");
+		try
+		{
+			FTPClient f = new FTPClient();
+		    f.connect("sokrates2.local.cs.hs-rm.de");
+		    f.login("hu000004", "1q2w3e");
+		    FTPListParseEngine engine = f.initiateListParsing("/in");
+	
+		    while (engine.hasNext()) 
+		    {
+		       FTPFile[] files = engine.getNext(100);  // "page size" you want
+		       //do whatever you want with these files, display them, etc.
+		       //expensive FTPFile objects not created until needed.
+		       
+		      // f.retrieveFile(arg0, arg1)
+		       
+		       for(int i=0;i<files.length;i++)
+		       {
+		    	   System.out.println(files[i].getName());
+		    	   
+		       }
+		       
+		       if(files.length>0)
+		       {
+		    	   System.out.println(files[0].toString());
+		    	   File file = new File("in/temp.edi");
+		    	   OutputStream out = new FileOutputStream(file);
+		    	   
+		    	   if(f.retrieveFile("in/"+files[0].getName(), out))
+		    	   {
+		    		   System.out.println("success");
+		    		   return file.getName();
+		    	   }
+		    	   else
+		    		   System.out.println("failure");		    		   
+		       }
+		    }
+		    
+		    f.disconnect();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return null;
+		
+		/*File f = new File("in");
 		if(f.isDirectory())
 		{
 			String[] files = f.list(new FilenameFilter() {
@@ -33,6 +85,6 @@ public class ReadInbox
 			return files[0];
 		}
 		else
-			return null;
+			return null;*/
 	}
 }
---
+++ trunk/iMallLiferant/src/paper4all/wsdl/WriteToOutboxResponse.java
@@ -0,0 +1,54 @@
+
+package paper4all.wsdl;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for write-to-outboxResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="write-to-outboxResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="success-result" type="{http://www.w3.org/2001/XMLSchema}boolean"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "write-to-outboxResponse", propOrder = {
+    "successResult"
+})
+public class WriteToOutboxResponse {
+
+    @XmlElement(name = "success-result")
+    protected boolean successResult;
+
+    /**
+     * Gets the value of the successResult property.
+     * 
+     */
+    public boolean isSuccessResult() {
+        return successResult;
+    }
+
+    /**
+     * Sets the value of the successResult property.
+     * 
+     */
+    public void setSuccessResult(boolean value) {
+        this.successResult = value;
+    }
+
+}
