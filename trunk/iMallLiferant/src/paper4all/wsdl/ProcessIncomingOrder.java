bf7f8d57dc6e4c7383282a91ee01a0e80aeb69f5
florinail <florinail@5e4c1358-b78c-702d-5bc4-760e88c5cea0>
2010-07-13 11:33:05 +0000

    process orders

---
+++ trunk/iMallLiferant/src/paper4all/wsdl/ProcessIncomingOrder.java
@@ -0,0 +1,60 @@
+
+package paper4all.wsdl;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for process-incoming-order complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="process-incoming-order">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="xmlOrderContent" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "process-incoming-order", propOrder = {
+    "xmlOrderContent"
+})
+public class ProcessIncomingOrder {
+
+    protected String xmlOrderContent;
+
+    /**
+     * Gets the value of the xmlOrderContent property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getXmlOrderContent() {
+        return xmlOrderContent;
+    }
+
+    /**
+     * Sets the value of the xmlOrderContent property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setXmlOrderContent(String value) {
+        this.xmlOrderContent = value;
+    }
+
+}
