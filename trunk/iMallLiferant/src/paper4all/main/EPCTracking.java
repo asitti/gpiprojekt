24ceec539a610284a2943d0bdce39f1826013bf8
darie17 <darie17@5e4c1358-b78c-702d-5bc4-760e88c5cea0>
2010-07-19 10:42:08 +0000

    sfasfd

--- trunk/iMallLiferant/src/paper4all/main/EPCTracking.java
+++ trunk/iMallLiferant/src/paper4all/main/EPCTracking.java
@@ -2,6 +2,7 @@ package paper4all.main;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -47,10 +48,30 @@ public class EPCTracking {
 						ResultSet rset = stmt.executeQuery("select sgtin from epc where gln='" + gln + "' and vk_datum='" + date + "'");
 						List<String> sgtins = new ArrayList<String>();
 						
+						int counter = 0; 
+						
 				        while (rset.next()) 
 				        {
-				        	System.out.println(rset.getString(1));
-				        	sgtins.add(rset.getString(1));
+				        	/*System.out.println(rset.getString(1));
+				        	sgtins.add(rset.getString(1));*/
+				        	
+				        	String temp = rset.getString(1);
+				        	String gtin = getGtinFromSgtin(temp);
+				        	if(gtin.equals("2965197100101") ||
+				        			gtin.equals("2965197100200") ||
+				        			gtin.equals("2965197100309") ||
+				        			gtin.equals("2965197100408") ||
+				        			gtin.equals("2965197100507"))
+				        	{
+				        		sgtins.add(temp);
+				        		System.out.println(temp);
+				        	}
+				        	counter++;
+				        }
+				        
+				        if(counter == 0)
+				        {
+				        	System.out.println("No results. Please check your input.");
 				        }
 				        
 				        if(!sgtins.isEmpty())
@@ -78,7 +99,8 @@ public class EPCTracking {
 		}
 		catch(Exception e)
 		{
-			e.printStackTrace();
+			//e.printStackTrace();
+			System.out.println("Internal error.");
 		}
 	}
 
@@ -92,28 +114,13 @@ public class EPCTracking {
 		EPCTracking epc = new EPCTracking();
 		epc.track();
 		
-		/*int anzahlSgtin;
-		Scanner scan = new Scanner(System.in);
-		System.out.println("anzahl sgtins: ");
-		anzahlSgtin = scan.nextInt();
-		//List
-		
-		for(int i=0; i<anzahlSgtin; i++)
-		{
-			
-		}*/
-		
 	    //Storage storage = new StorageService().getStoragePort();
 		//SgtinWhereAbouts res = storage.getSGTINWhereaboutsFromStorage(null);
 		//List<SgtinContainer> sgtinRes = res.getSgtin();
-		
-
 	}
 	
 	public void clearScreen()
 	{
-		//for(int i = 0; i < 35; i++)
-			//System.out.println();
 		System.out.flush();
 		System.out.println("Usage: get [date] [gln]");
 	}
@@ -135,5 +142,30 @@ public class EPCTracking {
 		}
 		return null;
 	}
+	
+	/**
+	 * @author Andreas Otto
+	 * @param sgtin
+	 * @return
+	 */
+	public String getGtinFromSgtin(String sgtin) 
+	{
+	       BigInteger bigInt = new BigInteger(sgtin, 10);
+
+	       String binarySGtin = bigInt.toString(2);
+	       String compPre = binarySGtin.substring(12, 39);
+	       String itemNum = binarySGtin.substring(39, 56);
+
+	       final BigInteger cPre = new BigInteger(compPre, 2);
+	       final BigInteger iNum = new BigInteger(itemNum, 2);
+
+	       itemNum = iNum.toString(10);
+	       compPre = cPre.toString(10);
+
+	       while (itemNum.length() < 5) {
+	           itemNum = "0" + itemNum;
+	       }
+	       return compPre + itemNum;
+    }
 
 }
